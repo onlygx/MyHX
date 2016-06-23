bdf4d809637dbe5f10488418a2f2a5c046336005
dingwei <411923983@qq.com>
2016-06-23 14:42:10 +0800

    测试提交

--- app/src/main/java/com/example/viewpagerdemo/ui/jlfragmenwork/actvity/LoginActivity.java
+++ app/src/main/java/com/example/viewpagerdemo/ui/jlfragmenwork/actvity/LoginActivity.java
@@ -165,7 +165,7 @@ public class LoginActivity extends JLBaseActivity implements View.OnClickListene
 
 
     /**
-     * 登录
+     * 登录1
      */
     public void login() {
         if (verfication()) {
